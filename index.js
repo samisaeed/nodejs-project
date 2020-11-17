664273f969449f87d2599b324212c79f54d057f1
mahmud al saeed <samisaeed496@gmail.com>
2020-11-17 18:32:16 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // dependencies
 const http = require('http');
+const url = require('url');
 // app Object - module scaffolding
 const app = {};
 // configuration
@@ -15,9 +16,19 @@ app.createServer = () => {
     })
 };
 app.handelRegRes = (req, res) => {
+    //request handling
+    // get the url parse it
+    const parsedUrl = url.parse( req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+    console.log(queryStringObject);
     res.end('hello world lhkj');
 };
 
 
 // start server
 app.createServer();
+
